8931982c670317808d19741ac7855123a2ed17ab
ShohagBasak <shohagbasak225@gmail.com>
2026-02-02 23:45:40 +0600

    some changes at voteredirect

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -30,7 +30,7 @@ export const router = createBrowserRouter([
                 loader: () => fetch('/dc.json')
             },
             {
-                path: 'minecraft/vote/:id',
+                path: 'minecraft/:voteId',
                 Component: VoteRedirect
             }
         ]
